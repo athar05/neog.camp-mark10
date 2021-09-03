e8b5604e96cf80113365fc3f65f53a03acbb87b1
athar05 <atharshakeel96@gmail.com>
2021-09-04 01:32:25 +0530

    final app

---
+++ app.js
@@ -0,0 +1,45 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes =[2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function checkBillAndCashGivenAmount() {
+
+  message.style.display = "none";
+
+
+if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value){
+
+        const amountToBeReturned = (cashGiven.value - billAmount.value);
+        calculateChange(amountToBeReturned);
+
+    } else {
+        showMessage("Cash Given Should Be Greater Than Bill Amount");
+    }
+
+} else {
+    showMessage("Please Enter A Valid Input");
+}
+
+});
+
+function calculateChange(amountToBeReturned) {
+    for (var i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc (
+            amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+
+
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg; 
+}
