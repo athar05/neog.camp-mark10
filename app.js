51d62766ddd7baf0bce6fab6254873a6da124e81
athar05 <atharshakeel96@gmail.com>
2021-09-06 22:55:55 +0530

    final changes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+const checkButton = document.getElementById("check-button");
+const message = document.getElementById("error-message");
 
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
 
-checkButton.addEventListener("click", function checkBillAndCashGivenAmount() {
-  message.style.display = "none";
+function checkBillAndCashGivenAmount() {
+  const billAmount = document.getElementById("bill-amount");
+  const cashGiven = document.getElementById("cash-given");
 
   if (billAmount.value > 0) {
+    console.log("cash: " + cashGiven.value);
+    console.log("bill: " + billAmount.value);
     if (cashGiven.value >= billAmount.value) {
       const amountToBeReturned = cashGiven.value - billAmount.value;
       calculateChange(amountToBeReturned);
@@ -19,17 +19,39 @@ checkButton.addEventListener("click", function checkBillAndCashGivenAmount() {
   } else {
     showMessage("Please Enter A Valid Input");
   }
-});
+}
 
-function calculateChange(amountToBeReturned) {
-  for (var i = 0; i < availableNotes.length; i++) {
-    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-    amountToBeReturned = amountToBeReturned % availableNotes[i];
-    noOfNotes[i].innerText = numberOfNotes;
-  }
+checkButton.addEventListener("click", checkBillAndCashGivenAmount);
+
+function calculateChange() {
+  /*
+    calculate balance - loop over currency/denomination array
+    get the modulo and display value on element
+    update the balance variable
+  */
+
+  const billAmount = document.getElementById("bill-amount");
+  const cashGiven = document.getElementById("cash-given");
+
+  let balanceAmount = cashGiven.value - billAmount.value;
+
+  availableNotes.forEach((note) => {
+    const count = Math.trunc(balanceAmount / note);
+    const elementID = "currency-value-" + note;
+    const element = document.getElementById(elementID);
+
+    if (count > 0) element.innerText = count;
+    else element.innerText = "-";
+
+    balanceAmount = balanceAmount - count * note;
+  });
 }
 
 function showMessage(msg) {
   message.style.display = "block";
   message.innerText = msg;
+
+  setTimeout(() => {
+    message.style.display = "none";
+  }, 3000);
 }
